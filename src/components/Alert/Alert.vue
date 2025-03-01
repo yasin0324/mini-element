3baf3248a836c657f1e4ba861717a8ccf60749b0
yasin <1848737490@qq.com>
2025-03-01 17:07:54 +0800

    feat:Alert组件

---
+++ src/components/Alert/Alert.vue
@@ -0,0 +1,45 @@
+<template>
+  <Transition>
+    <div
+      class="me-alert"
+      :class="{
+        [`me-alert--${type}`]: type,
+        [`me-alert--${effect}`]: effect,
+      }"
+      v-if="alertShow"
+    >
+      <div class="me-alert__content">
+        <span><slot /></span>
+      </div>
+      <div class="me-alert__close" v-if="closable" @click="closeAlert">
+        <Icon icon="xmark" />
+      </div>
+    </div>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Icon from "../Icon/Icon.vue";
+import type { AlertProps, AlertEmits } from "./type";
+
+defineOptions({
+  name: "meAlert",
+});
+
+withDefaults(defineProps<AlertProps>(), {
+  type: "info",
+  effect: "light",
+  closable: true,
+});
+const emits = defineEmits<AlertEmits>();
+
+// Alert展示状态
+const alertShow = ref<boolean>(true);
+
+// 关闭Alert
+const closeAlert = () => {
+  alertShow.value = false;
+  emits("close");
+};
+</script>
