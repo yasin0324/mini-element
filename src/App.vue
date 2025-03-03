b6f3a126fcc9e1f5919a8e56746af36faca967f6
yasin <1848737490@qq.com>
2025-03-03 21:18:25 +0800

    feat:Tooltip-支持click和hover动态事件绑定

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import Tooltip from "./components/Tooltip/Tooltip.vue";
 import Button from "./components/Button/Button.vue";
+
+const trigger = ref<any>("click");
+setTimeout(() => {
+  trigger.value = "hover";
+}, 2000);
 </script>
 
 <template>
-  <Tooltip>
+  <Tooltip placement="bottom" :trigger="trigger">
     <Button>Tooltip</Button>
     <template #content>
       <h1>helloWorld</h1>
--- src/components/Tooltip/Tooltip.vue
+++ src/components/Tooltip/Tooltip.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="me-tooltip">
-    <div class="me-tooltip__trigger" ref="triggerNode" @click="togglePopper">
+  <div class="me-tooltip" v-on="outerEvents">
+    <div class="me-tooltip__trigger" ref="triggerNode" v-on="events">
       <slot />
     </div>
     <div v-if="isOpen" class="me-tooltip__popper" ref="popperNode">
@@ -14,7 +14,7 @@
 <script setup lang="ts">
 import type { TooltipProps, TooltipEmits } from "./type";
 import { createPopper, type Instance } from "@popperjs/core";
-import { ref, watch } from "vue";
+import { ref, watch, reactive } from "vue";
 
 defineOptions({
   name: "meTooltip",
@@ -22,6 +22,7 @@ defineOptions({
 
 const props = withDefaults(defineProps<TooltipProps>(), {
   placement: "bottom",
+  trigger: "hover",
 });
 const emits = defineEmits<TooltipEmits>();
 // 内容区域是否展示
@@ -33,11 +34,45 @@ const popperNode = ref<HTMLElement | null>(null);
 
 // popper实例
 let popperInstance: null | Instance = null;
+let events: Record<string, any> = reactive({});
+let outerEvents: Record<string, any> = reactive({});
 
 const togglePopper = () => {
   isOpen.value = !isOpen.value;
   emits("visible-change", isOpen.value);
 };
+
+const open = () => {
+  isOpen.value = true;
+  emits("visible-change", isOpen.value);
+};
+
+const close = () => {
+  isOpen.value = false;
+  emits("visible-change", isOpen.value);
+};
+
+const attachEvents = () => {
+  if (props.trigger === "hover") {
+    events["mouseenter"] = open;
+    outerEvents["mouseleave"] = close;
+  } else if (props.trigger === "click") {
+    events["click"] = togglePopper;
+  }
+};
+attachEvents();
+
+watch(
+  () => props.trigger,
+  (newTrigger, oldTrigger) => {
+    if (newTrigger !== oldTrigger) {
+      // 事件清空
+      events = {};
+      outerEvents = {};
+      attachEvents();
+    }
+  }
+);
 
 watch(
   isOpen,
