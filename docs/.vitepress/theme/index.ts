a7496171b507ca1745d6970b0e836d0fdd489147
yasin <1848737490@qq.com>
2025-03-05 16:24:45 +0800

    feat:Vitepress自定义主题样式

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,17 @@
 import DefaultTheme from "vitepress/theme";
 import { library } from "@fortawesome/fontawesome-svg-core";
+import { ElementPlusContainer } from "@vitepress-demo-preview/component";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import "@vitepress-demo-preview/component/dist/style.css";
 
 import "../../../src/styles/index.css";
+import "./custom.css";
 
 library.add(fas);
 
-export default DefaultTheme;
+export default {
+  ...DefaultTheme,
+  enhanceApp({ app }) {
+    app.component("demo-preview", ElementPlusContainer);
+  },
+};
