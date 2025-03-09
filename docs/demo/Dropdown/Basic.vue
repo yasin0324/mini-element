362f06e4aff81e22bcc009c802de73b920a909d2
yasin <1848737490@qq.com>
2025-03-09 22:05:25 +0800

    文档样式修改

--- docs/demo/Dropdown/Basic.vue
+++ docs/demo/Dropdown/Basic.vue
@@ -14,21 +14,24 @@ import { ref } from "vue";
 const options = ref([
   {
     label: "选项1",
-    key: "1"
+    key: "1",
   },
   {
     label: "选项2",
-    key: "2"
+    key: "2",
   },
   {
     label: "选项3",
-    key: "3"
-  }
+    key: "3",
+  },
 ]);
 </script>
 
 <style>
-.block {
-  margin-bottom: 20px;
+.vp-doc .vitepress-demo-preview-preview ul,
+.vp-doc .vitepress-demo-preview-preview ol {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
 }
 </style>
