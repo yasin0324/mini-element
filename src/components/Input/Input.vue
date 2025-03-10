b267dd37579d3f28300e3855b175f114f7310d42
yasin <1848737490@qq.com>
2025-03-10 22:02:13 +0800

    feat:Input清除输入值功能

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -22,10 +22,19 @@
         <span class="me-input__prefix" v-if="$slots.prefix">
           <slot name="prefix" />
         </span>
-        <input :type="type" :disabled="disabled" class="me-input__inner" @input="handleInput" />
+        <input
+          :type="type"
+          :disabled="disabled"
+          class="me-input__inner"
+          @input="handleInput"
+          v-model="innerValue"
+          @focus="handleFocus"
+          @blur="handleBlur"
+        />
         <!-- suffix slot -->
-        <span class="me-input__suffix" v-if="$slots.suffix">
-          <slot name="suffix"></slot>
+        <span class="me-input__suffix" v-if="$slots.suffix || showClear">
+          <slot name="suffix" />
+          <Icon v-if="showClear" icon="circle-xmark" class="me-input__clear" @click="clear" />
         </span>
       </div>
       <!-- append slot -->
@@ -35,14 +44,22 @@
     </template>
     <!-- textarea -->
     <template v-else>
-      <textarea class="me-textarea__wrapper" :disabled="disabled" @input="handleInput"></textarea>
+      <textarea
+        class="me-textarea__wrapper"
+        :disabled="disabled"
+        @input="handleInput"
+        v-model="innerValue"
+        @focus="handleFocus"
+        @blur="handleBlur"
+      ></textarea>
     </template>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { InputProps, InputEmits } from "./type";
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
+import Icon from "../Icon/Icon.vue";
 
 defineOptions({
   name: "meInput",
@@ -52,9 +69,27 @@ const props = withDefaults(defineProps<InputProps>(), {
 });
 const emits = defineEmits<InputEmits>();
 
+// 输入框的值
 const innerValue = ref(props.modelValue);
+// 是否聚焦
+const isFocus = ref(false);
+// 展示清楚按钮
+const showClear = computed(
+  () => props.clearable && !props.disabled && innerValue.value && isFocus.value
+);
 
 const handleInput = () => {
+  emits("update:modelValue", innerValue.value);
+};
+const handleFocus = () => {
+  isFocus.value = true;
+};
+const handleBlur = () => {
+  isFocus.value = false;
+};
+// 清除
+const clear = () => {
+  innerValue.value = "";
   emits("update:modelValue", innerValue.value);
 };
 
