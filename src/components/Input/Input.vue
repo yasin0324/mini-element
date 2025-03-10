f6bfe34d7a02fa3c165da1e81086c647e3239054
yasin <1848737490@qq.com>
2025-03-11 00:09:14 +0800

    fix:Input-修复清除失效问题

--- src/components/Input/Input.vue
+++ src/components/Input/Input.vue
@@ -41,9 +41,19 @@
           @blur="handleBlur"
         />
         <!-- suffix slot -->
-        <span class="me-input__suffix" v-if="$slots.suffix || showClear || showPasswordArea">
+        <span
+          class="me-input__suffix"
+          v-if="$slots.suffix || showClear || showPasswordArea"
+          @click="keepFocus"
+        >
           <slot name="suffix" />
-          <Icon v-if="showClear" icon="circle-xmark" class="me-input__clear" @click="clear" />
+          <Icon
+            v-if="showClear"
+            icon="circle-xmark"
+            class="me-input__clear"
+            @click="clear"
+            @mousedown.prevent="NOOP"
+          />
           <Icon
             v-if="showPasswordArea && passwordVisible"
             icon="eye"
@@ -134,6 +144,11 @@ const handleBlur = (e: FocusEvent) => {
   isFocus.value = false;
   emits("blur", e);
 };
+const keepFocus = () => {
+  inputRef.value?.focus();
+};
+// 这个函数是个占位符，因为点击清除图标的时候会先触发 blur 事件，handleBlur 中会把 isFocus 置位 false，导致 showClear也变成 false，所以图标就会消失，clear 就不会被触发
+const NOOP = () => {};
 // 清除
 const clear = () => {
   innerValue.value = "";
