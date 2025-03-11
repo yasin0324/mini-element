c01bc604cd4a30607c93238ec803fbb62424b39e
yasin <1848737490@qq.com>
2025-03-11 18:34:27 +0800

    feat:Switch组件基本完成

---
+++ docs/demo/Switch/Switch.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="me-switch"
+    :class="{
+      [`me-switch--${size}`]: size,
+      'is-disabled': disabled,
+      'is-checked': checked,
+    }"
+  >
+    <input
+      class="me-switch__input"
+      type="checkbox"
+      role="switch"
+      :name="name"
+      :disabled="disabled"
+    />
+    <div class="me-switch__core">
+      <div class="me-switch__core-action"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import type { SwitchProps, SwitchEmits } from "./type";
+
+defineOptions({
+  name: "meSwitch",
+  inheritAttrs: false,
+});
+
+const props = defineOptions<SwitchProps>();
+const emits = defineEmits<SwitchEmits>();
+
+const innerValue = ref(props.modelValue);
+const checked = computed(() => innerValue.value);
+</script>
---
+++ docs/demo/Switch/type.ts
@@ -0,0 +1,21 @@
+export interface SwitchProps {
+  // 绑定值，必须等于active-value或inactive-value，默认为Boolean类型
+  modelValue: string;
+  // switch大小
+  size?: "small" | "large";
+  //   是否禁用
+  disabled?: boolean;
+  //   switch打开时的文字描述
+  activeText?: string;
+  // switch的状态为off时的文字描述
+  inactiveText?: string;
+  // switch对应的name属性
+  name?: string;
+  // input的id
+  id?: string;
+}
+
+export interface SwitchEmits {
+  (e: "change", value: boolean): void;
+  (e: "update:modelValue", value: boolean): void;
+}
