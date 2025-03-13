c96fd913067965e901f4caba1680581d038bd1f6
yasin <1848737490@qq.com>
2025-03-13 18:06:23 +0800

    解决部署bug

--- docs/demo/Dropdown/Manual.vue
+++ docs/demo/Dropdown/Manual.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="manual block">
-    <me-button @click="showme-dropdown">显示下拉菜单</me-button>
-    <me-button @click="hideme-dropdown" style="margin-left: 10px">隐藏下拉菜单</me-button>
+    <me-button @click="showDropdown">显示下拉菜单</me-button>
+    <me-button @click="hideDropdown" style="margin-left: 10px">隐藏下拉菜单</me-button>
     <div style="margin-top: 10px">
-      <me-dropdown :menuOptions="options" manual ref="me-dropdownRef">
+      <me-dropdown :menuOptions="options" manual ref="DropdownRef">
         <me-button>手动控制的下拉菜单</me-button>
       </me-dropdown>
     </div>
@@ -13,28 +13,28 @@
 <script setup>
 import { ref } from "vue";
 
-const me-dropdownRef = ref(null);
+const DropdownRef = ref(null);
 const options = ref([
   {
     label: "选项1",
-    key: "1"
+    key: "1",
   },
   {
     label: "选项2",
-    key: "2"
+    key: "2",
   },
   {
     label: "选项3",
-    key: "3"
-  }
+    key: "3",
+  },
 ]);
 
-const showme-dropdown = () => {
-  me-dropdownRef.value?.show();
+const showDropdown = () => {
+  DropdownRef.value?.show();
 };
 
-const hideme-dropdown = () => {
-  me-dropdownRef.value?.hide();
+const hideDropdown = () => {
+  DropdownRef.value?.hide();
 };
 </script>
 
