d2f6763a62e6e77bf19264aacd99ad46ecbdfa3e
yasin <1848737490@qq.com>
2025-03-13 14:28:17 +0800

    feat:Form组件初步实现

---
+++ src/components/Form/FormItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="me-form-item">
+    <label class="me-form-item__label">
+      <slot name="label" :label="label"> {{ label }}</slot>
+    </label>
+    <div class="me-form-item__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { FormItemProps } from "./types";
+
+defineOptions({
+  name: "meFormItem",
+});
+
+defineProps<FormItemProps>();
+</script>
